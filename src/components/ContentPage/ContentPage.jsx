4fb1aad340750a472e7e3caba3754c88f09e67f1
Deva0813 <devanand151101@gmail.com>
2023-09-25 20:01:36 +0530

    title comp

--- src/components/ContentPage/ContentPage.jsx
+++ src/components/ContentPage/ContentPage.jsx
@@ -1,39 +1,15 @@
-import ContStyle from "./ContentPage.module.css";
+import TitleCard from "../TitleCard/TitleCard";
+import style from "./ContentPage.module.css";
 
-export const ContentPage = () => {
-    return (
-        <div className={ContStyle.main}>
-            <div className={ContStyle.container}>
-                <button className={ContStyle.con_button}>{"Back"}</button>
-            </div>
-            <div className={ContStyle.container}>
-                <div className={ContStyle.titleBox}>
-                    <div className={ContStyle.com_logo}></div>
-                    <div className={ContStyle.title}>
-                        <div className={ContStyle.titleTxt}>
-                            <div className={ContStyle.titlebtn}>
-                                CosIQ
-                                <div className="">
-                                    <button>Save</button>
-                                    <button>Share</button>
-                                </div>
-                            </div>
-                            <div className="">Delhi</div>
-                        </div>
-                        <div className={ContStyle.tags}>
-                            <hr />
-                            <p>
-                                {" "}
-                                <span># BEAUTY & PERSONAL CARE</span>{" "}
-                                <span># SHARK TANK</span> # FMCG
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+const ContentPage = () => {
+	return (
+		<div className={style.main}>
+			<div className={style.container}>
+				<button className={style.con_button}>{"Back"}</button>
+			</div>
+			<TitleCard/>
+		</div>
+	);
 };
-
 
 export default ContentPage;
