10fec7b30a361021a4395faa994a9647f00eb2c7
Deva0813 <devanand151101@gmail.com>
2023-09-26 10:30:33 +0530

    Content Section basic functions

--- src/components/ContentSection/ContentSection.jsx
+++ src/components/ContentSection/ContentSection.jsx
@@ -1,23 +1,63 @@
-import {useState,useEffect} from 'react';
-import style from "./ContentSection.module.css"
+import { useState, useEffect } from "react";
+import style from "./ContentSection.module.css";
 
 const ContentSection = () => {
-    const [activeBtn , setActiveBtn] = useState("pitch")
+  const [activeBtn, setActiveBtn] = useState("pitch");
 
-    const handleNavigation = (e) =>{
-        setActiveBtn(e.target.id)
-    }
+  useEffect(() => {
+    var ele = document.getElementById(activeBtn);
+    var btns = document.querySelectorAll("#btns_div button");
+    btns.forEach((element) => {
+      element.classList.remove(style.active);
+    });
+    ele.classList.add(style.active);
+  }, [activeBtn]);
 
-    return (
-        <div className={style.container}>
-            <div className={style.sec_nav_buttons}>
-                <button id='pitch' onClick={handleNavigation} >Pitch</button>
-                <button id='details' onClick={handleNavigation} >Details</button>
-                <button id='discussion' onClick={handleNavigation} >Discussion</button>
-                <button id='updates' onClick={handleNavigation} >Updates</button>
-            </div>
+  const handleNavigation = (e) => {
+    setActiveBtn(e.target.id);
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.sec_nav_buttons} id="btns_div">
+        <button id="pitch" onClick={handleNavigation}>
+          Pitch
+        </button>
+        <button id="details" onClick={handleNavigation}>
+          Details
+        </button>
+        <button id="discussion" onClick={handleNavigation}>
+          Discussion
+        </button>
+        <button id="updates" onClick={handleNavigation}>
+          Updates
+        </button>
+      </div>
+
+      <div className={style.sec_cont}>
+        <div className={style.content}>
+          {activeBtn == "pitch" ? "Pitch" : null}
+          {activeBtn == "details" ? "Details" : null}
+          {activeBtn == "discussion" ? "Discussion" : null}
+          {activeBtn == "updates" ? "Updates" : null}
         </div>
-    );
+        <div className={style.doc_sec}>
+          <h2>Documents</h2>
+          <button className={style.doc_file_box}>
+            CosIQ.zip
+            <img
+              src="/pepcorns_assignment/images/download.svg"
+              alt="down"
+              height={20}
+            />
+          </button>
+          <div className={style.invest_sec}>
+            hello
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContentSection;
